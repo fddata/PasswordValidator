bb01e5b0d1875212f9b75bae3a120f28831e7d55
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-09-10 19:29:06 +0100

    adds to Validator and Rule classes

---
+++ Validator/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validator.IRules;
+
+namespace Validator
+{
+    internal class LengthRule : IRule
+    {
+        private int _target;
+
+        public LengthRule(int target)
+        {
+            this._target = target;
+        }
+
+        bool IRule.validate(string input)
+        {
+            return input.Length >= _target;
+        }
+    }
+}
---
+++ Validator/ValidatorRunner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Validator.IRules;
+
+namespace Validator
+{
+    public class ValidatorRunner
+    {
+
+        private List<IRule> _rules = new List<IRule>();
+
+        public ValidatorRunner() { }
+
+        public void AddRule(IRule rule)
+        {
+            this._rules.Add(rule);
+        }
+
+
+        public bool ValidateInput(string input)
+        {
+            foreach(IRule rule in _rules)
+            {
+                if (!rule.validate(input))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+
+    }
+}
