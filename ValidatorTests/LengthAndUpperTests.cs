d0d5d68619428619a70172b1a0ab008b6d72836c
Fraser Douglas <fraser.j.douglas@gmail.com>
2020-09-10 20:27:01 +0100

    updates comments

--- ValidatorTests/LengthAndUpperTests.cs
+++ ValidatorTests/LengthAndUpperTests.cs
@@ -11,56 +11,64 @@ namespace ValidatorTests
         [TestMethod]
         public void LengthUpperPassIfBothOK()
         {
+            //arrange
             var validator = new Validator();
-
             validator.AddRule(new LengthRule(8));
             validator.AddRule(new UpperRule(3));
-
             var input = "ABC45678";
+
+            //act
             bool result = validator.ValidateInput(input);
 
+            //assert
             Assert.IsTrue(result);
         }
 
         [TestMethod]
         public void LengthUpperFailIfLengthFail()
         {
+            //arrange
             var validator = new Validator();
-
             validator.AddRule(new LengthRule(8));
             validator.AddRule(new UpperRule(3));
-
             var input = "ABC4567";
+            
+            //act
             bool result = validator.ValidateInput(input);
 
+            //assert
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void LengthUpperFailIfUpperFail()
         {
+            //arrange
             var validator = new Validator();
-
             validator.AddRule(new LengthRule(8));
             validator.AddRule(new UpperRule(3));
-
             var input = "ABc45678";
+
+            //act
             bool result = validator.ValidateInput(input);
 
+            //assert
             Assert.IsFalse(result);
         }
 
         [TestMethod]
         public void LengthUpperFailIfBothFail()
         {
+            //arrange
             var validator = new Validator();
-
             validator.AddRule(new LengthRule(8));
             validator.AddRule(new UpperRule(3));
-
             var input = "ABc4567";
+
+            //act
             bool result = validator.ValidateInput(input);
 
+            //assert
             Assert.IsFalse(result);
         }
     }
